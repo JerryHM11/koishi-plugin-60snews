c5b36509e450f8b5940a95dbca7c5542673cf9da
JerryHM11 <1299640312@qq.com>
2022-11-01 23:17:17 +0800

    修改部分代码

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { Context, Schema } from 'koishi';
+export interface Config {
+}
+export declare const name = "60snews";
+export declare const Config: Schema<Config>;
+export declare function apply(ctx: Context): void;
--- src/index.js
+++ src/index.js
@@ -4,18 +4,20 @@ exports.apply = exports.Config = exports.name = void 0;
 const koishi_1 = require("koishi");
 exports.name = '60snews';
 exports.Config = koishi_1.Schema.object({});
+
 function apply(ctx) {
     // write your plugin here
     ctx.command('news', "新闻资讯")
     .action(async ({ options }) => {
         try {
-            const data = await ctx.http.get('https://api.xiaolu520.cn/api/60s.php', {responseType: 'arraybuffer'})
-            console.log(koishi_1.segment.image(data));
+            const data = await ctx.http.get('https://api.xiaolu520.cn/api/60s.php', {responseType: 'jpeg'})
             return koishi_1.segment.image(data);
         } catch (error) {
             ctx.logger('tools').warn(error);
             return '请求失败。';
         }
     })
+
 }
+
 exports.apply = apply;
